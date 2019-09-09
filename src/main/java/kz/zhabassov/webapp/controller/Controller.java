f0cbd7992b46b0da5956bb2d8b96044e52d3e83c
Tamerlan Zhabassov <tamerlan_zhabassov@epam.com>
2019-09-09 11:56:53 +0600

    try

--- src/main/java/kz/zhabassov/webapp/controller/Controller.java
+++ src/main/java/kz/zhabassov/webapp/controller/Controller.java
@@ -32,7 +32,7 @@ public class Controller extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-       ConnectionPool.getInstance();
+      // ConnectionPool.getInstance();
     }
 
     @Override
